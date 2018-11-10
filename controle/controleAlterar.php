4dd63319e7b84f124f4c3b43931152f60bbec8d2
Luis Felipe A. Venezian <felipelipe_11@hotmail.com>
2018-11-10 19:50:14 -0200

    Comentário principal, arquivo de alteração.

---
+++ controle/controleAlterar.php
@@ -0,0 +1,7 @@
+<?php   
+    /*
+     * Aqui será implantado as regras de transição
+     * para desenvolvimento da area de alteração dos
+     * registros dos professores.
+     * @Author: Luis Felipe. 10/11/2018
+     */
